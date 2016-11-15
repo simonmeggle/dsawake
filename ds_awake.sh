1a726b90b5f8e2557b06d81bf8486ea6bd4eaff8
Simon <Simon@local.de>
2016-11-15 21:18:31 +0100

    first release

--- ds_awake.sh
+++ ds_awake.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+#title           :ds_awake.sh
+#description     :a modular script to keep Synology Diskstation (or any other s
+#                 bashable system running as long as all plugins are returning a
+#                 rc > 0. Disable plugins simply by putting a file "disabled" into
+#                 the plugin folder. Use pre/host hooks to execute additional 
+#                 tasks. Prehooks are able to interrupt the script. 
+#author		     :Simon Meggle 
+#date            :20161115
+#version         :0.1
+#usage		     :bash ds_awake.sh
+#==============================================================================
 
 ROOT=$(dirname `readlink -f $0`)
 LOGFILE="/var/log/$(basename $0).log"
@@ -10,6 +21,7 @@ log() {
 }
 
 function main() {
+	cd $ROOT
 	log "===== START $(basename $0) ====="
 	log "----- Calling pre-hooks"
 	call_modules prehooks || return 
@@ -28,6 +40,7 @@ function call_modules() {
 	MTYPE=$1
 	# shutdown DS only if var is still zero in the end
 	ALLOW_SHUTDOWN=0
+	echo $ALLOW_SHUTDOWN
 	if [ ! -d $ROOT/$MTYPE ]; then 
 		log "ERROR: $ROOT/$MTYPE does not exist. Exiting."
 		exit 1
@@ -36,11 +49,17 @@ function call_modules() {
 		module=$( basename $d | sed -e 's/\///' )
 		# skip the module if disabled 
 		[[ -f $MTYPE/$module/disabled ]] && log "$ALLOW_SHUTDOWN - module $module: ###disabled###" && continue
-		log "$ALLOW_SHUTDOWN - module $module: Starting..."
-		pushd $MTYPE/$module > /dev/null
-		bash start.sh 
-		let "ALLOW_SHUTDOWN=$ALLOW_SHUTDOWN+$?"
-		popd > /dev/null
+		if [ -r $MTYPE/$module/start.sh ]; then 
+			log "$ALLOW_SHUTDOWN - module $module: Starting..."
+			# evaluate return code of start.sh, not tee
+			set -o pipefail
+			bash $MTYPE/$module/start.sh | tee -a $LOGFILE 
+			let "ALLOW_SHUTDOWN=$ALLOW_SHUTDOWN+$?"
+			set +o pipefail
+		else
+			let "ALLOW_SHUTDOWN=$ALLOW_SHUTDOWN+1"
+			log "ERROR: $MTYPE/$module/start.sh could not be found!"
+		fi
 	done
 	log "$MTYPE result: $ALLOW_SHUTDOWN"
 	return $ALLOW_SHUTDOWN
@@ -49,7 +68,7 @@ function call_modules() {
 function godown() {
 	log "+++ Shutting down the system in 5 seconds! "
 	sleep 5
-	/sbin/poweroff
+	#/sbin/poweroff
 }
 
 main
